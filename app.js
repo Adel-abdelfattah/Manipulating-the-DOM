8c99537afc79a79f98d65b692071acdde0008bee
adel <adel.fantasy11@gmail.com>
2022-03-20 22:13:20 +0200

    dom

--- app.js
+++ app.js
@@ -1,49 +1,23 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-
-// Define Global Variables
 var sections = document.querySelectorAll("section");
 var navLinks = document.querySelectorAll("nav a");
 
-
-// End Global Variables
-//  Start Helper Functions
-
 onscroll = function () {
   var scrollPosition = document.documentElement.scrollTop;
-
   sections.forEach((section) => {
     if (
       scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
       scrollPosition <
         section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
     ) {
+      document.querySelectorAll("nav a").forEach((el) => {
+        el.classList.remove("active");
+      });
       var currentId = section.attributes.id.value;
-      removeAllActiveClasses();
-      addActiveClass(currentId);
+      var selector = `nav a[href="#${currentId}"]`;
+      document.querySelector(selector).classList.add("active");
     }
   });
 };
-
-
 
 
 navLinks.forEach((link) => {
@@ -52,36 +26,9 @@ navLinks.forEach((link) => {
     var currentId = e.target.attributes.href.value;
     var section = document.querySelector(currentId);
     var sectionPos = section.offsetTop;
-    // section.scrollIntoView({
-    //   behavior: "smooth",
-    // });
-
     window.scroll({
       top: sectionPos,
       behavior: "smooth",
     });
   });
 });
-//  *End Helper Functions
-
-
-// Begin Main Functions
-var removeAllActiveClasses = function () {
-  document.querySelectorAll("nav a").forEach((el) => {
-    el.classList.remove("active");
-  });
-};
-
-var addActiveClass = function (id) {
-  // console.log(id);
-  var selector = `nav a[href="#${id}"]`;
-  document.querySelector(selector).classList.add("active");
-};
-//  End Main Functions
-
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
